2d726394d11bb57999c72a10d0b1f4fe76cbc798
Hasan <hasan.doctype@gmail.com>
2021-05-04 15:06:28 +0400

    add JS Lesson1

---
+++ JSLessons/Lesson1/script.js
@@ -0,0 +1,28 @@
+//one line comment
+/*
+more line comment
+
+*/
+// document.write("Hi JS");
+// document.writeln("Hi JS");
+
+// alert("hi JS");
+// window.alert("hi JS");
+
+
+// console.log("hi JS");
+
+
+// document.write('HI JS'+"<br>");
+// debugger
+// document.write('HI JS'+"<br>");
+// debugger
+// document.wsfgdrite('HI JS'+"<br>");
+// debugger
+// document.write('HI JS'+"<br>");
+// debugger
+// document.write('HI JS'+"<br>");
+// debugger
+// document.write('HI JS'+"<br>");
+// debugger
+
