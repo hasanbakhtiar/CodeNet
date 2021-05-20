9463294c3221f9ca7efd7b122ff2f1683b73947d
Hasan <hasan.doctype@gmail.com>
2021-05-20 14:10:32 +0400

    add JS Lesson5

---
+++ JSLessons/Lesson5/main.js
@@ -0,0 +1,49 @@
+// ------------------------------------------------------------------------------------------------
+// switch,case,default
+// var infoNumb = "elovset qezenferli";
+
+
+// switch (infoNumb) {
+//         case 5:
+//         document.write("Ədədimiz 5-dir");
+//         break;
+
+//         case 6:
+//         document.write("Ədədimiz 6-dir");
+//         break;
+
+//         case "elovset qezenferli":
+//         document.write("Ədədimiz 10-dir");
+//         break;
+
+//         case 15:
+//         document.write("Ədədimiz 15-dir");
+//         break;
+
+//     default:
+//         document.write("Bele bir eded yoxdur");
+
+//         break;
+// }
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+
+// var infoText = "JavaScript New";
+
+// try{
+//     // document.wrisdfdste("JavaScript HI");
+//     throw("<br>"+"Istisnayi bir hal var"+"<br>");
+
+//     // document.wrsfsdfite(infoText + "<br>");
+
+
+// }catch(error){
+//     document.write("Xeta Var"+"<br/>");
+//     document.write(error);
+
+// }finally{
+//     document.write("<br>"+"Hər halda işləyir");
+
+// }
+// ------------------------------------------------------------------------------------------------
