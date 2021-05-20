9409f849442e0160453ca50ebabc4b68312963a3
Hasan <hasan.doctype@gmail.com>
2021-05-20 15:37:12 +0400

    add Function

---
+++ JSLessons/Lesson6/main.js
@@ -0,0 +1,44 @@
+// var infoNumb = 1;
+
+// while (infoNumb <= 50) {
+//     document.write(infoNumb + "<br>");
+//     infoNumb ++;
+// }
+
+// var infoNumb = 21;
+
+// while (infoNumb <= 50 && infoNumb > 20 ) {
+
+//    if(infoNumb>20 && infoNumb < 30){
+//     document.write(infoNumb+"JS0" + "<br>");
+//     infoNumb ++;
+//    }else if(infoNumb >= 30 && infoNumb <= 50){
+//     document.write(infoNumb+"JS1" + "<br>");
+//     infoNumb ++;
+//    }
+
+// }
+
+// var infoNumb = 1;
+
+// do{
+//     document.write(infoNumb + "<br>");
+//     infoNumb ++;
+// }while(infoNumb <= 50);
+
+
+for(var infoNumb = 1; infoNumb <=100; infoNumb++){
+
+    if (infoNumb == 5) {
+        continue;
+    }
+    document.write(infoNumb+ "<br>");
+    
+}
+
+// var infoArray = ["JS", "PHP", "Python"];
+
+// for(infoData in infoArray ){
+//     document.write(infoArray[infoData]+"<br>");
+// }
+
---
+++ JSLessons/Lesson7/main.js
@@ -0,0 +1,29 @@
+// function name(params){
+//             doing...
+// }
+
+
+// function funcInfo(){
+//     document.write("My Func <br>");
+// }
+
+// const infoData = function(){
+//     document.write("Info Function");
+// }
+
+
+
+
+// funcInfo ();
+// infoData();
+
+
+// (function infoFunc (a,b){
+//     var c = a+b;
+//     document.write(c);
+// })(5,10)
+
+
+function infoFunc(a) {
+    document.querySelector('p').innerHTML = "Hi" + a;
+}
