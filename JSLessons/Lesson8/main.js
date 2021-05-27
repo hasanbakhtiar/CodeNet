1f264be4ebb68c1452989d9408ec1a57578ff014
Hasan <hasan.doctype@gmail.com>
2021-05-27 15:47:42 +0400

    add array lesson

---
+++ JSLessons/Lesson8/main.js
@@ -0,0 +1,78 @@
+// Array
+// const infoArray = ["table","pen","door",5,true];
+// const  infoArray = new Array("table","pen","door",5,true);
+
+// document.write(infoArray);
+
+// --------------------------------------------------------------------------------
+
+// const infoArray = ["table","pen","door",5,true];
+// document.write(infoArray[0]+"<br>");
+// document.write(infoArray[1]+"<br>");
+// document.write(infoArray[2]+"<br>");
+// document.write(infoArray[3]+"<br>");
+// document.write(infoArray[4]+"<br>");
+
+
+// --------------------------------------------------------------------------------
+
+
+// var infoArray = ["table","pen","door",5,true];
+
+// infoArray[1] = "window";
+
+// document.write(infoArray);
+
+
+// --------------------------------------------------------------------------------
+
+
+// const infoArraySt = ["table","pen","door",5,true]; infoArrayNd = ["green","red",'white'];
+
+
+// document.write(infoArraySt+"<br>");
+// document.write(infoArrayNd+"<br>");
+
+// --------------------------------------------------------------------------------
+
+
+// const infoArraySt = ["table","pen","door",5,true];
+// infoArraySt['schoolthing'] = 'table';
+
+// document.write(infoArraySt['schoolthing']+"<br>");
+// --------------------------------------------------------------------------------
+
+// const infoArraySt = [];
+// infoArraySt[0] = 'table';
+// infoArraySt[1] = 'pen';
+// infoArraySt[2] = 'door';
+// infoArraySt[3] = 5;
+// infoArraySt[4] = true;
+
+// document.write(infoArraySt+"<br>");
+
+// --------------------------------------------------------------------------------
+
+
+// const infoArray = ['pen','door',['green','red'],'window',['sun','cloud','storm',['school','university'],'rain','wind']];
+
+// document.write(infoArray[0]+"<br>");
+// document.write(infoArray[2][0]+"<br>");
+// document.write(infoArray[4][3][1]+"<br>");
+
+// --------------------------------------------------------------------------------
+// const infoCar = {
+//         name: "Mersedes",
+//         model: 'GL',
+//         year: 2020,
+//         carBase: true,
+//         drive: function(event){
+//                 document.write("Hight"+event);
+//         }
+// }
+
+// document.write(infoCar.name+"<br>");
+// document.write(infoCar.model+"<br>");
+// document.write(infoCar.year+"<br>");
+// document.write(infoCar.carBase+"<br>");
+// document.write(infoCar.drive(" speed "));
