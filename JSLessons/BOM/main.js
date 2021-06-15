c0f9c95c517ef3d7d459bffa0f16bdeeac1ea213
Hasan <hasan.doctype@gmail.com>
2021-06-15 14:57:36 +0400

    add timing

---
+++ JSLessons/BOM/main.js
@@ -0,0 +1,23 @@
+// Window
+// https://www.w3schools.com/js/js_window.asp
+
+// Window Screen
+// https://www.w3schools.com/js/js_window_screen.asp
+
+// Location
+// https://www.w3schools.com/js/js_window_location.asp
+
+// History
+// https://www.w3schools.com/js/js_window_history.asp
+
+// Window Navigator
+// https://www.w3schools.com/js/js_window_navigator.asp
+
+// JavaScript Popup Boxes
+// https://www.w3schools.com/js/js_popup.asp
+
+// The setTimeout() Method
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+
+// The setInterval() Method
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_setinterval2
