69023e64a6ae3d41bea812208eedd471163627e9
Hasan <hasan.doctype@gmail.com>
2021-06-17 15:45:54 +0400

    JQ lesson3

---
+++ JSLessons/JQuery/lesson1/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    $("#demo").click(function(){
+        $("p").hide();
+    });
+   
+
+  });
+
+// Nə?
+   // Necə?
+        // Nəyi?
+           // Etsin? 
+
+
+
+// $(function(){
+    
+// $("h1"/*nə ilə tətbiq edən*/)/*nə zaman tətbiq edən*/.click(function(){
+//   $(this/*tetbiq olunan*/).hide()/*tetbiq eden hereket*/;
+// });
+// });
+ 
---
+++ JSLessons/JQuery/lesson3/main.js
@@ -0,0 +1,27 @@
+// Slide Down
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_down
+
+// Animate
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation1
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation1_multicss
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation1_relative
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation1_toggle
+
+// STOP
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_stop_slide
+// mouse-logitech-pebble-m350
+
+
+// Callback
+// https://www.w3schools.com/jquery/jquery_callback.asp
+
+
+// JQ CSS
+// https://www.w3schools.com/jquery/jquery_chaining.asp
+
+// JQ DOM
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_html_get
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_html_get
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_html_get
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_attr_get
+
