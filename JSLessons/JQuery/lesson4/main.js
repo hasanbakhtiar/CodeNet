9963c055623119d6e4c4a6e08578da3dd71d42a4
Hasan <hasan.doctype@gmail.com>
2021-06-22 15:57:36 +0400

    add jquery all selector

---
+++ JSLessons/JQuery/lesson4/main.js
@@ -0,0 +1,47 @@
+
+// Append   
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_append
+
+//Preappend 
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_prepend
+
+// before after
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_after
+
+// Remove  
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_remove
+
+// Empty
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_empty
+
+
+// AddClass
+// https://www.w3schools.com/jquery/jquery_css_classes.asp
+
+// RemoveClass
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_removeclass
+
+
+// ToggleClass
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dom_toggleclass
+
+// CSS
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_getcolor
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_setcolor
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_set_multiple
+
+// Dimension 
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dim_width_height
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_dim_innerwidth_height
+
+// Ancestors
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_parent
+
+// Descendants
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_children
+
+// Siblings
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_siblings
+
+// Filtering
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_first
